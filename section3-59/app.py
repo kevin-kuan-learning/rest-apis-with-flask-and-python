b38fb9357ad8e2b7a34d463ea7435af52809bb12
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-10-17 21:31:11 +0800

    finish endpoint

--- section3-59/app.py
+++ section3-59/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -23,27 +23,51 @@ stores = [
 # POST  /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>', methods=['GET'])
 def get_store(name):
-    pass
+    request_data = request.get_json()
+    # Iterate over stores
+    for store in stores:
+    # If the store name matches, return it
+        if store['name'].casefold() == name.casefold():
+            return jsonify(store)
+    # If none match, return an error message
+    return jsonify({'message': 'store not found'})
 
 # GET /store
 @app.route('/store', methods=['GET'])
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'].casefold() == name.casefold():
+            store['items'].append({
+                'name': request_data['name'],
+                'price': request_data['price'],
+            })
+            return jsonify(store) 
+    return jsonify({'message': 'store not found'})
+    
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-    pass
-
+    for store in stores:
+        if store['name'].casefold() == name.casefold():
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 app.run(port=50000)
